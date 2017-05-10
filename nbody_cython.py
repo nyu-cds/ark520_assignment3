69228ad1cb948fa7e0d220680067e66c7044fba9
akunas <drewkunas@gmail.com>
2017-05-10 02:19:54 -0400

    finished assignment 8

--- nbody_cython.py
+++ nbody_cython.py
@@ -1,5 +1,3 @@
-#%%cython
-
 """
 A working version
 
@@ -8,42 +6,101 @@ A working version
 from itertools import combinations
 import numpy as np
 cimport numpy as np
-cimport cython
+
+cdef float PI 
+PI = 3.14159265358979323
+
+cdef float SOLAR_MASS
+SOLAR_MASS = 4 * PI * PI
+
+cdef float DAYS_PER_YEAR
+DAYS_PER_YEAR = 365.24
 
 ctypedef np.float_t DTYPE_FLOAT_t
 ctypedef np.int_t DTYPE_INT_t
 
-@cython.boundscheck(False)
-cdef void to_do_simplified(int body1,
+BODIES_NAMES = ['sun', 'jupiter', 'saturn', 'uranus', 'neptune']
+KEYS = np.array(list(range(len(BODIES_NAMES))))
+POSITIONS = np.array([
+    [0.0, 0.0, 0.0],
+    [4.84143144246472090e+00,
+                 -1.16032004402742839e+00,
+                 -1.03622044471123109e-01],
+    [8.34336671824457987e+00,
+                4.12479856412430479e+00,
+                -4.03523417114321381e-01],
+    [1.28943695621391310e+01,
+                -1.51111514016986312e+01,
+                -2.23307578892655734e-01],
+    [1.53796971148509165e+01,
+                 -2.59193146099879641e+01,
+                 1.79258772950371181e-01],
+])
+
+VELOCITIES = np.array([
+    [0.0, 0.0, 0.0],
+    [1.66007664274403694e-03 * DAYS_PER_YEAR,
+                 7.69901118419740425e-03 * DAYS_PER_YEAR,
+                 -6.90460016972063023e-05 * DAYS_PER_YEAR],
+    [-2.76742510726862411e-03 * DAYS_PER_YEAR,
+                4.99852801234917238e-03 * DAYS_PER_YEAR,
+                2.30417297573763929e-05 * DAYS_PER_YEAR],
+    
+     [2.96460137564761618e-03 * DAYS_PER_YEAR,
+                2.37847173959480950e-03 * DAYS_PER_YEAR,
+                -2.96589568540237556e-05 * DAYS_PER_YEAR],
+    [2.68067772490389322e-03 * DAYS_PER_YEAR,
+                 1.62824170038242295e-03 * DAYS_PER_YEAR,
+                 -9.51592254519715870e-05 * DAYS_PER_YEAR],  
+])
+
+MASSES = np.array([
+SOLAR_MASS,
+9.54791938424326609e-04 * SOLAR_MASS,
+2.85885980666130812e-04 * SOLAR_MASS,
+4.36624404335156298e-05 * SOLAR_MASS,
+5.15138902046611451e-05 * SOLAR_MASS,
+])
+
+
+VISIT_SCHEDULE = list(combinations(KEYS, 2))
+VISIT_SCHEDULE = np.array(VISIT_SCHEDULE)
+#BODIES1, BODIES2= zip(*VISIT_SCHEDULE)
+#BODIES_KEYS = BODIES_INDICES
+#KEYS = BODIES_INDICES
+cdef float dt
+dt = 0.01
+
+    
+def to_do_simplified(int body1,
                      int body2,
                     np.ndarray[DTYPE_FLOAT_t, ndim=2] POSITIONS,
                     np.ndarray[DTYPE_FLOAT_t, ndim=2] VELOCITIES,
                     np.ndarray[DTYPE_FLOAT_t, ndim=1] MASSES,
-                    float dt
                     ):
     
-    cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] xyz1 = POSITIONS[body1]
-    cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] xyz2 = POSITIONS[body2]
+    xyz1 = POSITIONS[body1]
+    xyz2 = POSITIONS[body2]
 
-    cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] v1 = VELOCITIES[body1]
-    cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] v2 = VELOCITIES[body2]
+    v1 = VELOCITIES[body1]
+    v2 = VELOCITIES[body2]
 
-    cdef float m1 = MASSES[body1]
-    cdef float m2 = MASSES[body2]
+    m1 = MASSES[body1]
+    m2 = MASSES[body2]
                 
-    cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] d = xyz1 - xyz2
+    d = xyz1 - xyz2
     
-    cdef float mag = dt * (np.inner(d, d) ** (-1.5))
+    mag = dt * (np.inner(d, d) ** (-1.5))
 
-    cdef body1_velocity_arg = m2 * mag
+    body1_velocity_arg = m2 * mag
     VELOCITIES[body1] -= d * body1_velocity_arg
 
-    cdef body2_velocity_arg = m1 * mag   
+    body2_velocity_arg = m1 * mag   
     VELOCITIES[body2] += d * body2_velocity_arg
     
 
-@cython.boundscheck(False)    
-cdef void update_rs_for_body(int body,
+    
+def update_rs_for_body(int body,
                        np.ndarray[DTYPE_FLOAT_t, ndim=2] POSITIONS,
                        np.ndarray[DTYPE_FLOAT_t, ndim=2] VELOCITIES,
                        np.ndarray[DTYPE_FLOAT_t, ndim=1] MASSES,
@@ -51,8 +108,8 @@ cdef void update_rs_for_body(int body,
     POSITIONS[body] += dt * VELOCITIES[body]
 
     
-@cython.boundscheck(False)    
-cdef float report_energy( np.ndarray[DTYPE_FLOAT_t, ndim=2] POSITIONS,
+    
+def report_energy( np.ndarray[DTYPE_FLOAT_t, ndim=2] POSITIONS,
                    np.ndarray[DTYPE_FLOAT_t, ndim=2] VELOCITIES,
                    np.ndarray[DTYPE_FLOAT_t, ndim=1] MASSES,
                    np.ndarray[DTYPE_INT_t, ndim=1] KEYS,
@@ -60,13 +117,13 @@ cdef float report_energy( np.ndarray[DTYPE_FLOAT_t, ndim=2] POSITIONS,
     '''
         compute the energy and return it so that it can be printed
     '''
-    cdef float e = 0.0
-    cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] xyz1, xyz2, d
-    cdef float m1, m2
+    e = 0.0
     for (body1, body2) in VISIT_SCHEDULE:
         xyz1 = POSITIONS[body1]
         xyz2 = POSITIONS[body2]
 
+        v1 = VELOCITIES[body1]
+        v2 = VELOCITIES[body2]
 
         m1 = MASSES[body1]
         m2 = MASSES[body2]
@@ -84,8 +141,8 @@ cdef float report_energy( np.ndarray[DTYPE_FLOAT_t, ndim=2] POSITIONS,
 
 
 
-@cython.boundscheck(False)
-cdef void offset_momentum(int reference_index,
+
+def offset_momentum(int reference_index,
                    np.ndarray[DTYPE_FLOAT_t, ndim=2] POSITIONS,
                    np.ndarray[DTYPE_FLOAT_t, ndim=2] VELOCITIES,
                    np.ndarray[DTYPE_FLOAT_t, ndim=1] MASSES,
@@ -95,7 +152,7 @@ cdef void offset_momentum(int reference_index,
         ref is the body in the center of the system
         offset values from this reference
     '''
-    cdef np.ndarray p = np.array([0.0, 0.0, 0.0])
+    p = np.array([0.0, 0.0, 0.0])
     for body in KEYS:
         v = VELOCITIES[body]
         m = MASSES[body]
@@ -108,84 +165,21 @@ cdef void offset_momentum(int reference_index,
 
 
 
-cdef nbody(int loops, str reference, int iterations):
+def nbody(loops, reference, iterations):
     '''
         nbody simulation
         loops - number of loops to run
         reference - body at center of system
         iterations - number of timesteps to advance
     '''
-    # initialize stuff
-    cdef float PI 
-    PI = 3.14159265358979323
-
-    cdef float SOLAR_MASS
-    SOLAR_MASS = 4 * PI * PI
-
-    cdef float DAYS_PER_YEAR
-    DAYS_PER_YEAR = 365.24
-
-
-
-    BODIES_NAMES = ['sun', 'jupiter', 'saturn', 'uranus', 'neptune']
-    cdef np.ndarray[DTYPE_INT_t, ndim=1] KEYS = np.arange(5)
-    cdef np.ndarray[DTYPE_FLOAT_t, ndim=2] POSITIONS = np.array([
-        [0.0, 0.0, 0.0],
-        [4.84143144246472090e+00,
-                     -1.16032004402742839e+00,
-                     -1.03622044471123109e-01],
-        [8.34336671824457987e+00,
-                    4.12479856412430479e+00,
-                    -4.03523417114321381e-01],
-        [1.28943695621391310e+01,
-                    -1.51111514016986312e+01,
-                    -2.23307578892655734e-01],
-        [1.53796971148509165e+01,
-                     -2.59193146099879641e+01,
-                     1.79258772950371181e-01],
-    ])
-
-    cdef np.ndarray[DTYPE_FLOAT_t, ndim=2] VELOCITIES = np.array([
-        [0.0, 0.0, 0.0],
-        [1.66007664274403694e-03 * DAYS_PER_YEAR,
-                     7.69901118419740425e-03 * DAYS_PER_YEAR,
-                     -6.90460016972063023e-05 * DAYS_PER_YEAR],
-        [-2.76742510726862411e-03 * DAYS_PER_YEAR,
-                    4.99852801234917238e-03 * DAYS_PER_YEAR,
-                    2.30417297573763929e-05 * DAYS_PER_YEAR],
-
-         [2.96460137564761618e-03 * DAYS_PER_YEAR,
-                    2.37847173959480950e-03 * DAYS_PER_YEAR,
-                    -2.96589568540237556e-05 * DAYS_PER_YEAR],
-        [2.68067772490389322e-03 * DAYS_PER_YEAR,
-                     1.62824170038242295e-03 * DAYS_PER_YEAR,
-                     -9.51592254519715870e-05 * DAYS_PER_YEAR],  
-    ])
-
-    cdef np.ndarray[DTYPE_FLOAT_t, ndim=1] MASSES = np.array([
-    SOLAR_MASS,
-    9.54791938424326609e-04 * SOLAR_MASS,
-    2.85885980666130812e-04 * SOLAR_MASS,
-    4.36624404335156298e-05 * SOLAR_MASS,
-    5.15138902046611451e-05 * SOLAR_MASS,
-    ])
-
-
-    cdef np.ndarray[DTYPE_INT_t, ndim=2] VISIT_SCHEDULE = np.array(list(combinations(KEYS, 2)))
-
-    cdef float dt = 0.01
-    # ================
-    
-    
-    
-    
     # Set up global state
-    offset_momentum(BODIES_NAMES.index(reference), POSITIONS, VELOCITIES, MASSES, KEYS)
+    offset_momentum(0, POSITIONS, VELOCITIES, MASSES, KEYS)
 
+    print("HI3")
     for _ in range(loops):
         for _ in range(iterations):            
             for body1, body2 in VISIT_SCHEDULE:
-                to_do_simplified(body1, body2, POSITIONS, VELOCITIES, MASSES, dt)   
+                to_do_simplified(body1, body2, POSITIONS, VELOCITIES, MASSES)   
             for b in KEYS:
                 update_rs_for_body(b, POSITIONS, VELOCITIES, MASSES, dt)
         print(report_energy(POSITIONS, VELOCITIES, MASSES, KEYS, VISIT_SCHEDULE))
--- nbody_numba.py
+++ nbody_numba.py
@@ -1,168 +1,140 @@
-"""
-
-This was actually based off of nbody_iter.py, beacuse I didn't like my nbody_opt.py code
-
-
-"""
-
-from numba import jit
-import numpy as np
 import itertools
+import numpy as np
+from numba import jit, int32, float64, char, vectorize
 
-PI = 3.14159265358979323
-SOLAR_MASS = 4 * PI * PI
-DAYS_PER_YEAR = 365.24
+@vectorize([float64(float64, float64)])
+def vec_deltas(a, b):
+    # fast computation of deltas, as per assignment
+    return a - b
 
-BODIES = {
-    'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+@jit("float64(float64[:])")
+def l2_norm_len3(a):
+    # computes the L2 norm very very quickly
+    return a[0] * a[0] + a[1] * a[1] + a[2] * a[2]
 
-    'jupiter': ([4.84143144246472090e+00,
-                 -1.16032004402742839e+00,
-                 -1.03622044471123109e-01],
-                [1.66007664274403694e-03 * DAYS_PER_YEAR,
-                 7.69901118419740425e-03 * DAYS_PER_YEAR,
-                 -6.90460016972063023e-05 * DAYS_PER_YEAR],
-                9.54791938424326609e-04 * SOLAR_MASS),
+@jit("void(int32, int32, float64[:,:], float64[:,:], float64[:], float64)", nopython=True)
+def to_do(body1, body2, POSITIONS, VELOCITIES, MASSES, dt):
+    (xyz1, v1, m1) = POSITIONS[body1], VELOCITIES[body1], MASSES[body1]
+    (xyz2, v2, m2) = POSITIONS[body2], VELOCITIES[body2], MASSES[body2]
 
-    'saturn': ([8.34336671824457987e+00,
-                4.12479856412430479e+00,
-                -4.03523417114321381e-01],
-               [-2.76742510726862411e-03 * DAYS_PER_YEAR,
-                4.99852801234917238e-03 * DAYS_PER_YEAR,
-                2.30417297573763929e-05 * DAYS_PER_YEAR],
-               2.85885980666130812e-04 * SOLAR_MASS),
-
-    'uranus': ([1.28943695621391310e+01,
-                -1.51111514016986312e+01,
-                -2.23307578892655734e-01],
-               [2.96460137564761618e-03 * DAYS_PER_YEAR,
-                2.37847173959480950e-03 * DAYS_PER_YEAR,
-                -2.96589568540237556e-05 * DAYS_PER_YEAR],
-               4.36624404335156298e-05 * SOLAR_MASS),
-
-    'neptune': ([1.53796971148509165e+01,
-                 -2.59193146099879641e+01,
-                 1.79258772950371181e-01],
-                [2.68067772490389322e-03 * DAYS_PER_YEAR,
-                 1.62824170038242295e-03 * DAYS_PER_YEAR,
-                 -9.51592254519715870e-05 * DAYS_PER_YEAR],
-                5.15138902046611451e-05 * SOLAR_MASS)}
-
-VISIT_SCHEDULE = list(itertools.combinations(BODIES.keys(), 2))
-BODIES1, BODIES2= zip(*VISIT_SCHEDULE)
-BODIES_KEYS = list(BODIES.keys())
-dt = 0.01
-
-
-# alter data types to arrays
-# BODIES IS NOW SPLIT INTO BODIES1_XYZ, BODIES2_V, BODIES2_MASS
-BODIES_2_XYZ = np.array([BODIES[k][0] for k in BODIES_KEYS])
-BODIES_2_V = np.array([BODIES[k][1] for k in BODIES_KEYS])
-BODIES_2_MASS = np.array([BODIES[k][2] for k in BODIES_KEYS])
-
-BODIES_KEYS = np.array( [i for i, k in enumerate(BODIES_KEYS)])
-VISIT_SCHEDULE = list(itertools.combinations(BODIES_KEYS, 2))
-BODIES1, BODIES2= (np.array(it) for it in zip(*VISIT_SCHEDULE))
-
-@jit("Tuple((float64[:], float64[:], float64)(int64, float64[:,:], float64[:,:], float64[:])")
-def get_from_bodies(body, BODIES_2_XYZ=BODIES_2_XYZ, BODIES_2_V=BODIES_2_V, BODIES_2_MASS = BODIES_2_MASS):
-    return (BODIES_2_XYZ[body], BODIES_2_V[body], BODIES_2_MASS[body])
-
-@jit("void(int64, int64, float64)")
-def to_do(body1, body2, dt=dt):
-    (xyz1, v1, m1) = get_from_bodies(body1)
-    (xyz2, v2, m2) = get_from_bodies(body2)
-    d = xyz1 - xyz2
+    d = vec_deltas(xyz1, xyz2)
     
-    mag = dt * (np.sum(d*d) ** (-1.5))
+    mag = dt * (l2_norm_len3(d) ** (-1.5))
+
+    
 
     body1_velocity_arg = m2 * mag
-    v1 = v1 - d * body1_velocity_arg
-    #v1[0] -= dx * body1_velocity_arg
-    #v1[1] -= dy * body1_velocity_arg
-    #v1[2] -= dz * body1_velocity_arg
+    v1[:] -= d * body1_velocity_arg
 
-    body2_velocity_arg = m1 * mag   
-    v2 = v2 - d * body2_velocity_arg
-    #v2[0] += dx * body2_velocity_arg
-    #v2[1] += dy * body2_velocity_arg
-    #v2[2] += dz * body2_velocity_arg
 
-@jit("int64(int64, int64)")
-def update_rs_for_body(body, dt=dt):
-    (r, [vx, vy, vz], m) = get_from_bodies(body)
-    r[0] += dt * vx
-    r[1] += dt * vy
-    r[2] += dt * vz 
-    
-@jit("int64(int64[:])")
-def report_energy(BODIES_KEYS=BODIES_KEYS):
+    body2_velocity_arg = m1 * mag       
+    v2[:] += d * body1_velocity_arg
+
+@jit("void(int32, float64[:,:], float64[:,:], float64[:], float64)", nopython=True)
+def update_rs_for_body(body, POSITIONS, VELOCITIES, MASSES, dt):
+    (r, vxyz, m) = POSITIONS[body], VELOCITIES[body], MASSES[body]
+    r[:] += dt * vxyz
+
+@jit("float64(float64[:,:], float64[:,:], float64[:], int32[:])", nopython=True)
+def report_energy(POSITIONS, VELOCITIES, MASSES, BODIES_KEYS):
     '''
         compute the energy and return it so that it can be printed
     '''
-    e = 0.0
-    for (body1, body2) in VISIT_SCHEDULE:
-        ((x1, y1, z1), v1, m1) = get_from_bodies(body1)
-        ((x2, y2, z2), v2, m2) = get_from_bodies(body2)
+    e = float(0.0)
+    
+    for body1 in BODIES_KEYS:
+        for body2 in range(body1+1, len(BODIES_KEYS)):
 
-        (dx, dy, dz) = (x1-x2, y1-y2, z1-z2)
+            #(body1, body2) = VISIT_SCHEDULE[i, 0], VISIT_SCHEDULE[i, 1]
+            (xyz1, v1, m1) = POSITIONS[body1], VELOCITIES[body1], MASSES[body1]
+            (xyz2, v2, m2) = POSITIONS[body2], VELOCITIES[body2], MASSES[body2]
 
-        e -= (m1 * m2) / ((dx * dx + dy * dy + dz * dz) ** 0.5)
+            d = vec_deltas(xyz1, xyz2)
+            
+            # Amazingly the code as written is faster than this code below.
+            e -= (m1 * m2) / np.sqrt(l2_norm_len3(d))
+
         
     for body in BODIES_KEYS:
-        (r, v, m) = get_from_bodies(body)
-        e += m * np.sum(v*v) / 2.
+        (r, v, m) = POSITIONS[body], VELOCITIES[body], MASSES[body]
+        e += m * (l2_norm_len3(v)) / 2.
         
     return e
 
-@jit("int64(int64, int64[:])")
-def offset_momentum(ref, BODIES_KEYS=BODIES_KEYS):
+@jit("void(int32, float64[:,:], float64[:,:], float64[:], int32[:])", nopython=True)
+def offset_momentum(ref, POSITIONS, VELOCITIES, MASSES, BODIES_KEYS):
     '''
         ref is the body in the center of the system
         offset values from this reference
     '''
-    p = np.array([0,0,0], dtype='float64')
-                         
+    p = np.zeros(3)
     for body in BODIES_KEYS:
-        (r, v, m) = get_from_bodies(body)
-        p = p - v * m
-        #px -= vx * m
-        #py -= vy * m
-        #pz -= vz * m
+        (r, v, m) = POSITIONS[body], VELOCITIES[body], MASSES[body]
+        p -= v*m
         
-    (r, v, m) = ref
-    v = p / m
-    #v[0] = px / m
-    #v[1] = py / m
-    #v[2] = pz / m
+    (r, v, m) = POSITIONS[ref], VELOCITIES[ref], MASSES[ref]
+    
+    # Necessary syntax to allow memory sharing
+    v[:] = p / m
 
-def nbody(loops, reference, iterations, VISIT_SCHEDULE=VISIT_SCHEDULE,
-          BODIES=BODIES, BODIES_KEYS=BODIES_KEYS, BODIES1=BODIES1, BODIES2=BODIES2):
+@jit("void(int32, char, int32)")
+def nbody(loops, reference, iterations):
     '''
         nbody simulation
         loops - number of loops to run
         reference - body at center of system
         iterations - number of timesteps to advance
     '''
-    reference = 0
+    
+    # initialize stuff
+    # initialization done inside for the sake of jit and shared-memory issues
+    # ====================================
+    PI = 3.14159265358979323
+    SOLAR_MASS = 4 * PI * PI
+    DAYS_PER_YEAR = 365.24
+    BODIES_NAMES = ['sun', 'jupiter', 'saturn', 'uranus', 'neptune']
+    BODIES_KEYS = np.array( list(range(5)), dtype=np.int32)
+    POSITIONS = np.array([
+        [0.0, 0.0, 0.0],
+        [4.84143144246472090e+00,                 -1.16032004402742839e+00,                 -1.03622044471123109e-01],
+        [8.34336671824457987e+00,                4.12479856412430479e+00,                -4.03523417114321381e-01],
+        [1.28943695621391310e+01,                -1.51111514016986312e+01,                -2.23307578892655734e-01],
+        [1.53796971148509165e+01,                 -2.59193146099879641e+01,                 1.79258772950371181e-01],
+    ], dtype=np.float64)
+
+    VELOCITIES = np.array([
+        [0.0, 0.0, 0.0],
+        [1.66007664274403694e-03 * DAYS_PER_YEAR,                 7.69901118419740425e-03 * DAYS_PER_YEAR,                 -6.90460016972063023e-05 * DAYS_PER_YEAR],
+        [-2.76742510726862411e-03 * DAYS_PER_YEAR,                4.99852801234917238e-03 * DAYS_PER_YEAR,                2.30417297573763929e-05 * DAYS_PER_YEAR],
+        [2.96460137564761618e-03 * DAYS_PER_YEAR,                2.37847173959480950e-03 * DAYS_PER_YEAR,                -2.96589568540237556e-05 * DAYS_PER_YEAR],
+        [2.68067772490389322e-03 * DAYS_PER_YEAR,                 1.62824170038242295e-03 * DAYS_PER_YEAR,                 -9.51592254519715870e-05 * DAYS_PER_YEAR],  
+    ], dtype=np.float64)
+    VISIT_SCHEDULE = np.array(list(itertools.combinations(BODIES_KEYS, 2)), dtype=np.int32)
+
+    MASSES = np.array([ SOLAR_MASS, 9.54791938424326609e-04 * SOLAR_MASS, 2.85885980666130812e-04 * SOLAR_MASS, 4.36624404335156298e-05 * SOLAR_MASS, 5.15138902046611451e-05 * SOLAR_MASS,])
+    dt = 0.01
+    # ====================================
+    
+    
+    
+    
+    
     # Set up global state
-    offset_momentum(get_from_bodies(reference))
+    offset_momentum(BODIES_NAMES.index(reference), POSITIONS, VELOCITIES, MASSES, BODIES_KEYS)
 
     total_runs = loops * iterations
     for i, (body1, body2) in enumerate(itertools.cycle(VISIT_SCHEDULE)):
-        to_do(body1, body2)
+        to_do(body1, body2, POSITIONS, VELOCITIES, MASSES, dt)
+    
         
         if (i % iterations) == 0:
             for body in BODIES_KEYS:
-                update_rs_for_body(body)
+                update_rs_for_body(body, POSITIONS, VELOCITIES, MASSES, dt)
                 
         if (i % loops) == 0:
-            print(report_energy())
-            pass
-            
-        if i >= total_runs - 1:
-            break
+            print(report_energy(POSITIONS, VELOCITIES, MASSES, BODIES_KEYS))
+                  
 
-            
+
 if __name__ == '__main__':
-    nbody(10, 'sun', 2000)
+    nbody(100, 'sun', 20000)
